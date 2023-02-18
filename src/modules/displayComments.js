283ecbfc42d1a0f25097ebe9c382b72b459d6998
Menekam Rudy <menekamrudy@gmail.com>
2023-02-18 12:21:30 +0100

    Remove console.log and live demo

--- src/modules/displayComments.js
+++ src/modules/displayComments.js
@@ -3,7 +3,6 @@ import fetchComment from './fetchcomments.js';
 
 const displayComments = async (id) => {
   const comments = await fetchComment(id);
-  console.log(comments);
   commentsCounter(comments);
   comments.forEach((c) => {
     document.getElementById('all-comments').innerHTML += `<li>${c.creation_date} ${c.username}: ${c.comment}</li>`;
