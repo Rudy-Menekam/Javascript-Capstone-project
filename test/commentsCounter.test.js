e0526293fb8262c476f44d1b7d87e4c0c7baa4e7
alicemirigo92 <alicemirigo92@gmai.com>
2023-02-18 17:42:03 +0300

    Add test

--- test/commentsCounter.test.js
+++ test/commentsCounter.test.js
@@ -2,11 +2,19 @@
 import commentsCounter from '../src/modules/commentsCounter.js';
 
 describe('commentsCounter,', () => {
-  document.body.innerHTML = '<span id="c-total"></span>';
-  const count = document.getElementById('c-total');
-  const comments = [];
-  commentsCounter(comments);
   test('displays 0 when there are no comments', () => {
+    document.body.innerHTML = '<span id="c-total"></span>';
+    const count = document.getElementById('c-total');
+    const comments = [];
+    commentsCounter(comments);
     expect(count.textContent).toBe('0');
+  });
+
+  test('displays 0 when there are no comments', () => {
+    document.body.innerHTML = '<span id="c-total"></span>';
+    const count = document.getElementById('c-total');
+    const comments = [{}];
+    commentsCounter(comments);
+    expect(count.textContent).toBe('1');
   });
 });
